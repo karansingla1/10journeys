6e31ed708f7db4116272cf7abc11c0bbc2d0e679
Karan  Singla <singlakaran1994@gmail.com>
2019-04-12 12:08:45 +0530

    Updates

---
+++ precache-manifest.03d71e99d753b93689e6802fa14d9c82.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1414dcad68f18113dee9",
+    "url": "10journeys.com/static/css/main.07464758.chunk.css"
+  },
+  {
+    "revision": "1414dcad68f18113dee9",
+    "url": "10journeys.com/static/js/main.77ef99b8.chunk.js"
+  },
+  {
+    "revision": "8bb0efb3fa75fdd4080b",
+    "url": "10journeys.com/static/js/runtime~main.29e44d49.js"
+  },
+  {
+    "revision": "db4dbc89b6bd5ee9247e",
+    "url": "10journeys.com/static/css/2.674da93f.chunk.css"
+  },
+  {
+    "revision": "db4dbc89b6bd5ee9247e",
+    "url": "10journeys.com/static/js/2.83f95a8f.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "10journeys.com/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "10journeys.com/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "10journeys.com/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "10journeys.com/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "10journeys.com/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "950a8338392d0e2f871f26cbce0008ae",
+    "url": "10journeys.com/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c78438b8f1f8a9e72e7a8b68d93e983.js"
+  "10journeys.com/precache-manifest.03d71e99d753b93689e6802fa14d9c82.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("10journeys.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
