ea214422c684297211bc7587bc970b29e844d979
Karan  Singla <singlakaran1994@gmail.com>
2019-04-25 09:38:39 +0530

    added helmet to blogpost

--- src/Components/Blog/BlogpostComponent.js
+++ src/Components/Blog/BlogpostComponent.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { FacebookIcon,TwitterIcon,WhatsappIcon,} from 'react-share';
 import {FacebookShareButton,LinkedinShareButton,TwitterShareButton,WhatsappShareButton,
   } from 'react-share';
+import { Helmet } from "react-helmet";
+
 
 function Blogpost(props) {
 	if (props.post){	 
 		return (
 			<>
+
+			<Helmet>
+	            <title>{props.post.title}</title>
+	            <meta name="description" content={props.post.excerpt} />
+	            <meta property="og:image" content={props.post.featured_image}/>
+    			<meta property="og:image:secure_url" content={props.post.featured_image}/>
+    			<meta property="og:title" content={props.post.title}/>
+	            <meta property="og:image:alt" content={props.post.title}/>
+
+
+          	</Helmet>
+
 			<div className="container">
 			<div className="row">
 			<div className="col-12 col-md-8">
@@ -16,7 +30,8 @@ function Blogpost(props) {
 					Share this article.
 				</div>
 
-				<FacebookShareButton className="col-auto" url = {"10journeys.com/#/blog/" + props.post.ID + '/' + props.post.title.split(' ').join('-')}>
+				<FacebookShareButton className="col-auto" 
+				url = {"10journeys.com/#/blog/" + props.post.ID + '/' + props.post.title.split(' ').join('-')}>
 		          <FacebookIcon size={42} round={true} />
 	              </FacebookShareButton>
 
